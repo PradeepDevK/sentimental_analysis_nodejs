f9dc0dc588bc40f5d0718e97940fb538fa5f13d9
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-07-22 19:21:08 +0530

    Intial Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors =  require('cors');
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
